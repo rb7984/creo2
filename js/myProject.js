081ee31b7396cc8ddbc6b28cd32d52efef723c66
riccbarelli <99691825+rb7984@users.noreply.github.com>
2022-12-24 17:49:07 +0100

    OrbiControls

--- js/myProject.js
+++ js/myProject.js
@@ -1,26 +1,28 @@
 import * as th from './three.module.js';
+import { OrbitControls } from './OrbitControls.js';
 
-const scene = new THREE.Scene();
+const scene = new th.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new th.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-
+const renderer = new th.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.update();
+
+const geometry = new th.BoxGeometry( 1, 1, 1 );
+const material = new th.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new th.Mesh( geometry, material );
 
 scene.add( cube );
 
 camera.position.set(5,5,5);
 camera.lookAt(0,0,0);
 
-const light = new th.AmbientLight( 0x404040, 20);
-scene.add(light);
+// const light = new th.AmbientLight( 0x404040, 20);
+// scene.add(light);
 
 window.addEventListener('mousedown',function() {
     camera.position.z = 14;
@@ -28,8 +30,9 @@ window.addEventListener('mousedown',function() {
 
 function animate() {
 	requestAnimationFrame( animate );
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
+    controls.update();
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 };
 
