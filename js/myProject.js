9ab88e861b5b43c84875cdea912a256127fd743a
riccbarelli <99691825+rb7984@users.noreply.github.com>
2022-12-25 16:41:17 +0100

    import fbx

--- js/myProject.js
+++ js/myProject.js
@@ -1,35 +1,48 @@
 import * as THREE from './three.module.js';
-// import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0//OrbitControls.js';
+import {OrbitControls} from './OrbitControls.js';
+import {FBXLoader} from './FBXLoader.js';
+import { MTLLoader } from './MTLLoader.js'
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor("#ededed");
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
 
-scene.add( cube );
+var g2 = new THREE.PlaneBufferGeometry(2000, 2000, 8, 8);
+var m2 = new THREE.MeshBasicMaterial({ color: '#a18787', side: THREE.DoubleSide });
+var plane = new THREE.Mesh(g2, m2);
+plane.rotateX( - Math.PI / 2);
+
+scene.add(plane);
 
 camera.position.set(5,5,5);
 camera.lookAt(0,0,0);
 
-// const light = new THREE.AmbientLight( 0x404040, 20);
-// scene.add(light);
+const light = new THREE.AmbientLight( 0x404040, 20);
+scene.add(light);
 
-// window.addEventListener('mousedown',function() {
-//     camera.position.z = 14;
-// })
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const fbxLoader = new FBXLoader();
+fbxLoader.load('../assets/stand.fbx',
+(object) => {
+    object.translateY(0.01);
+    scene.add(object)
+}
+);
+
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	// cube.rotation.x += 0.01;
+	// cube.rotation.y += 0.01;
+    controls.update();
 	renderer.render( scene, camera );
 };
 
