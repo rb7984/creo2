3c8ca7e1df588fb6612b2dd344bdfe85978ecad0
riccbarelli <99691825+rb7984@users.noreply.github.com>
2022-12-25 18:28:50 +0100

    added fbx

--- js/myProject.js
+++ js/myProject.js
@@ -8,34 +8,48 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
 renderer.setClearColor("#ededed");
 renderer.setSize( window.innerWidth, window.innerHeight );
+
 document.body.appendChild( renderer.domElement );
 
+var axes = new THREE.AxesHelper(5);
+const grid = new THREE.GridHelper(20,10,'#ffffff','#ffffff');
+scene.add(axes);
+scene.add(grid);
 
 var g2 = new THREE.PlaneBufferGeometry(2000, 2000, 8, 8);
-var m2 = new THREE.MeshBasicMaterial({ color: '#a18787', side: THREE.DoubleSide });
+var m2 = new THREE.MeshStandardMaterial({ color: '#a18787', side: THREE.DoubleSide });
 var plane = new THREE.Mesh(g2, m2);
 plane.rotateX( - Math.PI / 2);
+plane.receiveShadow = true;
 
 scene.add(plane);
 
 camera.position.set(5,5,5);
 camera.lookAt(0,0,0);
 
-const light = new THREE.AmbientLight( 0x404040, 20);
+const light = new THREE.AmbientLight( 0x404040, 4.5);
 scene.add(light);
+const lightDir = new THREE.DirectionalLight(0x404040, 2);
+lightDir.position.set(20,20,20);
+lightDir.lookAt(0,0,0);
+lightDir.castShadow = true;
+const dlsh = new THREE.CameraHelper(lightDir.shadow.camera);
+scene.add(dlsh);
+scene.add(lightDir);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const fbxLoader = new FBXLoader();
-fbxLoader.load('../assets/stand.fbx',
+fbxLoader.load('../assets/stand2.fbx',
 (object) => {
     object.translateY(0.01);
+    object.castShadow = true;
     scene.add(object)
 }
 );
-
 
 function animate() {
 	requestAnimationFrame( animate );
